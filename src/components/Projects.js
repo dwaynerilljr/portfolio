1797d3cc0b811ef9932d0eb28bc6502b3e4e5f40
Dwayne Rill Jr <dwayne@dwaynerilljr.dev>
2022-12-19 21:32:29 -0500

    Updated Projects Section Updated Project section to reflect recently completed project.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { SiGithub } from 'react-icons/si'
 import { FiMonitor } from 'react-icons/fi'
-import xmas from '../christmas-app.png'
+import poke from '../poke-memory.png'
 
 export default function Projects() {
   return (
@@ -10,18 +10,17 @@ export default function Projects() {
             Projects
         </header>
         <div className='sm:col-span-2 col-span-1 flex flex-col gap-y-2'>
-            <img src={xmas} alt="" className='w-full' />
-            <header className='uppercase text-lg sm:text-xl'>Christmas List</header>
+            <img src={poke} alt="" className='w-full border border-white' />
+            <header className='uppercase text-lg sm:text-xl'>Pokémon Memory</header>
             <div className='flex justify-between items-center text-sm sm:text-base font-thin'>
                 <div className='flex gap-x-1 text-xs sm:gap-x-4'>
                     <p>HTML</p>
                     <p>CSS</p>
                     <p>React</p>
-                    <p>Tailwind</p>
                 </div>
                 <div className='hidden sm:flex gap-x-1 sm:gap-x-4 font-semibold text-teal-400'>
-                    <a href="https://github.com/dwaynerilljr/christmas-list-app" target="_blank" rel="noreferrer" className='focus:outline-teal-400 border-b border-transparent hover:border-b hover:border-b-white'>Github</a>
-                    <a href="https://xmas-list.netlify.app/" target="_blank" rel="noreferrer" className='focus:outline-teal-400 border-b border-transparent hover:border-b hover:border-b-white'>Demo</a>
+                    <a href="https://github.com/dwaynerilljr/pokemon-memory" target="_blank" rel="noreferrer" className='focus:outline-teal-400 border-b border-transparent hover:border-b hover:border-b-white'>Github</a>
+                    <a href="https://pok3-memory.netlify.app/" target="_blank" rel="noreferrer" className='focus:outline-teal-400 border-b border-transparent hover:border-b hover:border-b-white'>Demo</a>
                 </div>
                 <div className="flex sm:hidden gap-x-3 text-base">
                     <a href="https://www.google.com" target="_blank" rel="noreferrer" className='focus:outline-teal-400'><SiGithub /></a>
@@ -29,66 +28,7 @@ export default function Projects() {
                 </div>
             </div>
         </div>
-        <div className='sm:col-span-2 col-span-1 flex flex-col gap-y-2'>
-            <img src={xmas} alt="" className='w-full' />
-            <header className='uppercase text-lg sm:text-xl'>Christmas List</header>
-            <div className='flex justify-between items-center text-sm sm:text-base font-thin'>
-                <div className='flex gap-x-1 text-xs sm:gap-x-4'>
-                    <p>HTML</p>
-                    <p>CSS</p>
-                    <p>React</p>
-                    <p>Tailwind</p>
-                </div>
-                <div className='hidden sm:flex gap-x-1 sm:gap-x-4 font-semibold text-teal-400'>
-                    <a href="https://github.com/dwaynerilljr/christmas-list-app" target="_blank" rel="noreferrer" className='focus:outline-teal-400 border-b border-transparent hover:border-b hover:border-b-white'>Github</a>
-                    <a href="https://xmas-list.netlify.app/" target="_blank" rel="noreferrer" className='focus:outline-teal-400 border-b border-transparent hover:border-b hover:border-b-white'>Demo</a>
-                </div>
-                <div className="flex sm:hidden gap-x-3 text-base">
-                    <a href="https://www.google.com" target="_blank" rel="noreferrer" className='focus:outline-teal-400'><SiGithub /></a>
-                    <a href="https://xmas-list.netlify.app" target="_blank" rel="noreferrer" className='focus:outline-teal-400'><FiMonitor /></a>
-                </div>
-            </div>
-        </div>
-        <div className='sm:col-span-2 col-span-1 flex flex-col gap-y-2'>
-            <img src={xmas} alt="" className='w-full' />
-            <header className='uppercase text-lg sm:text-xl'>Christmas List</header>
-            <div className='flex justify-between items-center text-sm sm:text-base font-thin'>
-                <div className='flex gap-x-1 text-xs sm:gap-x-4'>
-                    <p>HTML</p>
-                    <p>CSS</p>
-                    <p>React</p>
-                    <p>Tailwind</p>
-                </div>
-                <div className='hidden sm:flex gap-x-1 sm:gap-x-4 font-semibold text-teal-400'>
-                    <a href="https://github.com/dwaynerilljr/christmas-list-app" target="_blank" rel="noreferrer" className='focus:outline-teal-400 border-b border-transparent hover:border-b hover:border-b-white'>Github</a>
-                    <a href="https://xmas-list.netlify.app/" target="_blank" rel="noreferrer" className='focus:outline-teal-400 border-b border-transparent hover:border-b hover:border-b-white'>Demo</a>
-                </div>
-                <div className="flex sm:hidden gap-x-3 text-base">
-                    <a href="https://www.google.com" target="_blank" rel="noreferrer" className='focus:outline-teal-400'><SiGithub /></a>
-                    <a href="https://xmas-list.netlify.app" target="_blank" rel="noreferrer" className='focus:outline-teal-400'><FiMonitor /></a>
-                </div>
-            </div>
-        </div>
-        <div className='sm:col-span-2 col-span-1 flex flex-col gap-y-2'>
-            <img src={xmas} alt="" className='w-full' />
-            <header className='uppercase text-lg sm:text-xl'>Christmas List</header>
-            <div className='flex justify-between items-center text-sm sm:text-base font-thin'>
-                <div className='flex gap-x-1 text-xs sm:gap-x-4'>
-                    <p>HTML</p>
-                    <p>CSS</p>
-                    <p>React</p>
-                    <p>Tailwind</p>
-                </div>
-                <div className='hidden sm:flex gap-x-1 sm:gap-x-4 font-semibold text-teal-400'>
-                    <a href="https://github.com/dwaynerilljr/christmas-list-app" target="_blank" rel="noreferrer" className='focus:outline-teal-400 border-b border-transparent hover:border-b hover:border-b-white'>Github</a>
-                    <a href="https://xmas-list.netlify.app/" target="_blank" rel="noreferrer" className='focus:outline-teal-400 border-b border-transparent hover:border-b hover:border-b-white'>Demo</a>
-                </div>
-                <div className="flex sm:hidden gap-x-3 text-base">
-                    <a href="https://www.google.com" target="_blank" rel="noreferrer" className='focus:outline-teal-400'><SiGithub /></a>
-                    <a href="https://xmas-list.netlify.app" target="_blank" rel="noreferrer" className='focus:outline-teal-400'><FiMonitor /></a>
-                </div>
-            </div>
-        </div>
+        
     </section>
   )
 }
