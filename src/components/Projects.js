defa5cee9aee28a06d3f18b233d8d0442f1465ee
Dwayne Rill Jr <dwayne@dwaynerilljr.dev>
2023-06-03 17:13:36 -0400

    Made so long ago, don't remember

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,7 @@ import { FiMonitor } from 'react-icons/fi'
 import poke from '../poke-memory.png'
 import nba from '../nba-quiz.png'
 import todo from '../todo.png'
+import weather from '../weather.png'
 
 export default function Projects() {
   return (
@@ -65,6 +66,25 @@ export default function Projects() {
                 <div className="flex sm:hidden gap-x-3 text-base">
                     <a href="https://github.com/dwaynerilljr/todo-list" target="_blank" rel="noreferrer" className='focus:outline-teal-400'><SiGithub /></a>
                     <a href="https://fluffy-ganache-d60fd6.netlify.app/" target="_blank" rel="noreferrer" className='focus:outline-teal-400'><FiMonitor /></a>
+                </div>
+            </div>
+        </div>
+        <div className='sm:col-span-2 col-span-1 flex flex-col gap-y-2'>
+            <img src={weather} alt="" className='w-full h-full border border-white' />
+            <header className='uppercase text-lg sm:text-xl'>Hey What's the Weather?</header>
+            <div className='flex justify-between items-center text-sm sm:text-base font-thin'>
+                <div className='flex gap-x-1 text-xs sm:gap-x-4'>
+                    <p>HTML</p>
+                    <p>Bootstrap</p>
+                    <p>JavaScript</p>
+                </div>
+                <div className='hidden sm:flex gap-x-1 sm:gap-x-4 font-semibold text-teal-400'>
+                    <a href="https://github.com/dwaynerilljr/weather-app" target="_blank" rel="noreferrer" className='focus:outline-teal-400 border-b border-transparent hover:border-b hover:border-b-white'>Github</a>
+                    <a href="https://heywhatstheweather.netlify.app/" target="_blank" rel="noreferrer" className='focus:outline-teal-400 border-b border-transparent hover:border-b hover:border-b-white'>Demo</a>
+                </div>
+                <div className="flex sm:hidden gap-x-3 text-base">
+                    <a href="https://github.com/dwaynerilljr/weather-app" target="_blank" rel="noreferrer" className='focus:outline-teal-400'><SiGithub /></a>
+                    <a href="https://heywhatstheweather.netlify.app/" target="_blank" rel="noreferrer" className='focus:outline-teal-400'><FiMonitor /></a>
                 </div>
             </div>
         </div>
